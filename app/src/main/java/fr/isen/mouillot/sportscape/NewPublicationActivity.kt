da4f93fc3d9d05d1dc7e9c058118f56eeb2d4b58
klervi10 <klervi.muros-le-rouzic@isen.yncrea.fr>
2024-03-25 16:52:17 +0100

    ajouter du texte comme publication

--- app/src/main/java/fr/isen/mouillot/sportscape/NewPublicationActivity.kt
+++ app/src/main/java/fr/isen/mouillot/sportscape/NewPublicationActivity.kt
@@ -14,25 +14,20 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
 import fr.isen.mouillot.sportscape.ui.theme.SportScapeTheme
 
 class NewPublicationActivity : ComponentActivity() {
@@ -72,7 +67,7 @@ fun TopBar(title: String, onBackPress: () -> Unit) {
                 modifier = Modifier.size(48.dp)
             ) {
                 Icon(
-                    painterResource(id = R.drawable.croix128),
+                    painterResource(id = R.drawable.croix),
                     contentDescription = "Retour page principale",
                     tint = Color.White
                 )
