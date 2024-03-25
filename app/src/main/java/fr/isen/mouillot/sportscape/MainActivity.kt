72ccfd050c63499a522407f638d8dd1a0a01dc6d
jade <jademouillot@gmail.com>
2024-03-25 10:36:01 +0100

    firebase erreur

--- app/src/main/java/fr/isen/mouillot/sportscape/MainActivity.kt
+++ app/src/main/java/fr/isen/mouillot/sportscape/MainActivity.kt
@@ -1,6 +1,8 @@
 package fr.isen.mouillot.sportscape
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,11 +12,34 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import fr.isen.mouillot.sportscape.ui.theme.SportScapeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Pour écrire des données
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+
+        // Pour lire des données
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue(String::class.java)
+                Log.d(TAG, "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d(TAG, "Failed to read value.", error.toException())
+            }
+        })
+
         setContent {
             SportScapeTheme {
                 // A surface container using the 'background' color from the theme
