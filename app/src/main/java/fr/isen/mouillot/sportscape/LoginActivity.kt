a20ad68903a7d36fa9e3e8d85a6793cb18911d13
georgiaclemencon <georgia.clemencon@isen.yncrea.fr>
2024-03-26 08:22:00 +0100

    Login Register Fonctionnel mais pas joli

--- app/src/main/java/fr/isen/mouillot/sportscape/LoginActivity.kt
+++ app/src/main/java/fr/isen/mouillot/sportscape/LoginActivity.kt
@@ -1,63 +1,157 @@
 package fr.isen.mouillot.sportscape
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
 import fr.isen.mouillot.sportscape.ui.theme.SportScapeTheme
 
-class LoginActivity : ComponentActivity() {
+class EmailPasswordActivity : ComponentActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+
         setContent {
+
+            var currentUser = remember {
+                mutableStateOf(auth.currentUser)
+            }
+
+            //logIn("doudo@gmail.com", "doudou", currentUser)
+
+
             SportScapeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    //color = MaterialTheme.colors.background,
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            text = "Hello, Jetpack Compose!",
-                            //style = MaterialTheme.typography.h5,
-                            modifier = Modifier.padding(16.dp)
-                        )
-                    }
+                   // Greeting2(currentUser.value?.email ?: "No user")
+                    LoginScreen(currentUser, this::logIn, this::startActivity)
                 }
             }
         }
     }
+
+    private fun startActivity(activityClass: Class<*>) {
+        val intent = Intent(this, activityClass)
+        startActivity(intent)
+    }
+    private fun logIn(email: String, password: String, currentUser: MutableState<FirebaseUser?>) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d(TAG, "signInWithEmail:success")
+                currentUser.value = auth.currentUser
+                startActivity(MainActivity::class.java)
+                finish()
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w(TAG, "signInWithEmail:failure", task.exception)
+                Toast.makeText(
+                    baseContext, "Authentication failed.", Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+//    private fun Register(
+//        email: String,
+//        password: String,
+//        currentUser: MutableState<FirebaseUser?>
+//    ) {
+//        auth.signInWithEmailAndPassword(email, password)
+//            .addOnCompleteListener(this) { task ->
+//                if (task.isSuccessful) {
+//                    // Sign in success, update UI with the signed-in user's information
+//                    Log.d(TAG, "signInWithEmail:success")
+//                    currentUser.value = auth.currentUser
+//                    startActivity(RegisterActivity::class.java)
+//                    finish()
+//
+//                } else {
+//                    // If sign in fails, display a message to the user.
+//                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+//                    Toast.makeText(
+//                        baseContext,
+//                        "Authentication failed.",
+//                        Toast.LENGTH_SHORT,
+//                    ).show()
+//
+//                }
+//            }
+//    }
 }
 
+
 @Composable
-fun Greeting3(name: String, modifier: Modifier = Modifier) {
+fun LoginScreen(currentUser: MutableState<FirebaseUser?>, logIn: (String, String, MutableState<FirebaseUser?>) -> Unit,startActivity: (Class<*>) -> Unit) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(currentUser.value?.email ?: "No user")
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = { logIn(email, password, currentUser) }) {
+            Text("Log in")
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = {startActivity(RegisterActivity::class.java) }) {
+            Text("Create account")
+
+        }
+    }
+}
+
+
+
+@Composable
+fun Greeting2(name: String) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!"
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview3() {
-    SportScapeTheme {
-        Greeting3("Android")
-    }
-}
---
+++ app/src/main/java/fr/isen/mouillot/sportscape/RegisterActivity.kt
@@ -0,0 +1,123 @@
+package fr.isen.mouillot.sportscape
+
+import android.content.ContentValues
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig.TAG
+import fr.isen.mouillot.sportscape.ui.theme.SportScapeTheme
+
+class RegisterActivity : ComponentActivity() {
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+        setContent {
+            SportScapeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    RegisterScreen(currentUser = remember { mutableStateOf(null) }, this::Register)
+                }
+            }
+        }
+    }
+    private fun startActivity(activityClass: Class<*>) {
+        val intent = Intent(this, activityClass)
+        startActivity(intent)
+    }
+    private fun Register(
+        email: String,
+        password: String,
+        currentUser: MutableState<FirebaseUser?>
+    ) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    currentUser.value = auth.currentUser
+                    startActivity(MainActivity::class.java)
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    //updateUI(null)
+                }
+            }
+    }
+}
+
+@Composable
+fun RegisterScreen(currentUser: MutableState<FirebaseUser?>, register: (String, String, MutableState<FirebaseUser?>) -> Unit){
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = { register(email, password, currentUser) }) {
+            Text("Create account")
+        }
+    }
+}
+
+@Composable
+fun Greeting3(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    SportScapeTheme {
+        Greeting3("Android")
+    }
+}
