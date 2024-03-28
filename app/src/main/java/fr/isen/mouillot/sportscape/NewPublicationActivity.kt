292e7bd9c6df4f7ebcc539e4e52185b9981de683
klervi10 <klervi.muros-le-rouzic@isen.yncrea.fr>
2024-03-28 17:15:05 +0100

    remettre le code bien

--- app/src/main/java/fr/isen/mouillot/sportscape/NewPublicationActivity.kt
+++ app/src/main/java/fr/isen/mouillot/sportscape/NewPublicationActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -55,13 +56,7 @@ import kotlinx.coroutines.delay
 import java.util.Date
 import java.util.UUID
 
-private const val REQUEST_CODE_PICK_IMAGES = 123
-
 class NewPublicationActivity : ComponentActivity() {
-    private var selectedPhotos: MutableList<Uri> =
-        mutableListOf() // Déclarer une variable globale pour stocker les photos sélectionnées
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var auth = FirebaseAuth.getInstance()
@@ -78,44 +73,20 @@ class NewPublicationActivity : ComponentActivity() {
                         if (currentUser != null && currentUser.email != null){
                             DescriptionSection(
                                 postPost = ::postPost,
-                                selectedPhotos = selectedPhotos,
                                 user = currentUser.email ?: "NO EMAIL"
                             )
                         } else {
                             Text("Vous devez être connecté pour publier un post.")
                         }
                     }
-
-//                        DescriptionSection(selectedPhotos)
                 }
             }
         }
     }
-
-
-    //    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//        if (requestCode == REQUEST_CODE_PICK_IMAGES && resultCode == RESULT_OK) {
-//            // Récupère les Uri des photos sélectionnées depuis l'intention
-//            val clipData = data?.clipData
-//            if (clipData != null) {
-//                for (i in 0 until clipData.itemCount) {
-//                    val uri = clipData.getItemAt(i).uri
-//                    this.selectedPhotos.add(uri) // Mettre à jour la variable globale avec les photos sélectionnées
-//                }
-//            } else {
-//                val uri = data?.data
-//                if (uri != null) {
-//                    selectedPhotos.add(uri) // Mettre à jour la variable globale avec les photos sélectionnées
-//                }
-//            }
-//        }
-//    }
     private fun startActivity(activity: Class<*>) {
         val intent = Intent(this, activity)
         startActivity(intent)
     }
-
 
     private fun postPost(description: String, user: String, returnValue: MutableState<Boolean>) {
         val database =
@@ -140,7 +111,7 @@ fun TopBar(title: String) {
     val context = LocalContext.current
 
     Surface(
-        modifier = Modifier.fillMaxWidth(), color = Color(51, 104, 188)
+        modifier = Modifier.fillMaxWidth(), color = Color.Blue
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -170,154 +141,71 @@ fun TopBar(title: String) {
 @Composable
 fun DescriptionSection(
     postPost: (String, String, MutableState<Boolean>) -> Unit,
-    selectedPhotos: List<Uri>,
     user: String
 ) {
     var descriptionText by remember { mutableStateOf("") }
     val publishSnackbarVisible = remember { mutableStateOf(false) }
+    val database =
+        FirebaseDatabase.getInstance("https://sportscape-38027-default-rtdb.europe-west1.firebasedatabase.app/")
 
-    // État pour stocker les photos sélectionnées
-    var selectedPhotos by remember { mutableStateOf<List<Uri>>(emptyList()) }
-    Column {
-        Text("Description : ", fontWeight = FontWeight.Bold)
-        Surface(
-            modifier = Modifier.fillMaxWidth(),
-            color = Color(232, 232, 232),
-            border = BorderStroke(2.dp, Color.Black),
-            shape = MaterialTheme.shapes.medium,
+    Column(
+        modifier = Modifier.fillMaxHeight(), // Occupies the entire height of the parent
+        verticalArrangement = Arrangement.SpaceBetween // Aligns children vertically with space between them
+    ) {
+        Column(
+            modifier = Modifier.weight(1f)
+                .padding(horizontal = 16.dp),
         ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
+            Text("Description : ", fontWeight = FontWeight.Bold)
+            Surface(
+                modifier =
+                Modifier.fillMaxWidth(),
+                color = Color(232, 232, 232),
+                border = BorderStroke(2.dp, Color.Black),
+                shape = MaterialTheme.shapes.medium,
             ) {
-                BasicTextField(
-                    value = descriptionText,
-                    onValueChange = { descriptionText = it },
-                    modifier = Modifier.fillMaxWidth()
-                )
-
-                if (descriptionText.isEmpty()) {
-                    Text(
-                        text = "Entrez votre texte ici",
-                        color = Color.Gray,
-                        fontSize = 16.sp,
-                        modifier = Modifier.align(Alignment.CenterStart)
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 10.dp)
+                ) {
+                    BasicTextField(
+                        value = descriptionText,
+                        onValueChange = { descriptionText = it },
+                        modifier = Modifier.fillMaxWidth()
                     )
+
+                    if (descriptionText.isEmpty()) {
+                        Text(
+                            text = "  Entrez votre texte ici",
+                            color = Color.Gray,
+                            fontSize = 16.sp,
+                            modifier = Modifier.align(Alignment.CenterStart)
+                        )
+                    }
                 }
             }
         }
-        Button(
-            onClick = {
-                postPost(descriptionText, user, publishSnackbarVisible)
-            },
-            modifier = Modifier.size(48.dp),
-//                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp) // Add padding to the bottom for space
         ) {
-            Text(
-                text = "Publier", color = Color.White
-            )
-
-
+            Button(
+                onClick = {
+                    postPost(descriptionText, user, publishSnackbarVisible)
+                },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+            ) {
+                Text(
+                    text = "Publier", color = Color.White
+                )
+            }
         }
     }
-
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-
-
-
-
-//            // Bouton "Ouvrir la galerie"
-//            Button(
-//                onClick = {
-//                    openGallery(activity = LocalContext as Activity)
-//                }, modifier = Modifier.align(Alignment.CenterHorizontally)
-//            ) {
-//                Text("Ouvrir la galerie")
-//            }
-//
-//            // Affiche les photos sélectionnées
-//            selectedPhotos.forEach { photo ->
-//                Image(
-//                    painter = rememberImagePainter(photo),
-//                    contentDescription = null,
-//                    modifier = Modifier.size(100.dp)
-//                )
-//            }
-
-
-        // Bouton "Publier"
-
-
-//                if (descriptionText.isNotEmpty()) {
-//                    // Publication de la description dans la base de données Firebase
-//                    val ref = database.getReference("publications").push()
-//                    ref.child("description").setValue(descriptionText)
-//
-//                    // Publier les photos dans le stockage Firebase
-//                    selectedPhotos.forEachIndexed { index, uri ->
-//                        val storageRef =
-//                            FirebaseStorage.getInstance().reference.child("images/${ref.key}/photo$index")
-//                        val uploadTask = storageRef.putFile(uri)
-//
-//                        // Gérer le succès ou l'échec du téléchargement de chaque photo
-//                        uploadTask.addOnSuccessListener { taskSnapshot ->
-//                            // Photo téléchargée avec succès
-//                            // Vous pouvez ajouter ici des actions supplémentaires si nécessaire
-//                        }.addOnFailureListener { exception ->
-//                            // Erreur lors du téléchargement de la photo
-//                            Log.e(
-//                                "FirebaseStorage",
-//                                "Erreur lors du téléchargement de la photo: ${exception.message}"
-//                            )
-//                        }
-//                    }
-//
-//                    publishSnackbarVisible.value = true
-//                }
-
-
-//                AddPostSection()
-
-
-    }
-
-    if (publishSnackbarVisible.value) {
-        PublishConfirmationSnackbar(
-            context = LocalContext.current, message = "Message publié avec succès"
-        )
-    }
 }
-
-
-fun openGallery(activity: Activity) {
-    val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
-        addCategory(Intent.CATEGORY_OPENABLE)
-        type = "image/*"
-        putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-    }
-    activity.startActivityForResult(intent, REQUEST_CODE_PICK_IMAGES)
-}
-
-//fun publish(description: String, photos: List<Uri>) {
-//    // Publie la description dans la base de données Firebase
-//    val database = FirebaseDatabase.getInstance()
-//    val ref = database.getReference("publications").push()
-//    ref.child("description").setValue(description)
-//
-//    // Publie les photos dans le stockage Firebase
-//    photos.forEachIndexed { index, uri ->
-//        val storageRef =
-//            FirebaseStorage.getInstance().reference.child("images/${ref.key}/photo$index")
-//        storageRef.putFile(uri)
-//    }
-//}
 
 @Composable
 fun PublishConfirmationSnackbar(
@@ -336,3 +224,4 @@ fun PublishConfirmationSnackbar(
         SnackbarHost(snackbarHostState)
     }
 }
+
