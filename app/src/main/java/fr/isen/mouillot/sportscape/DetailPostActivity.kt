3984b65e1e1fb51717def349d66ae34db003c955
georgiaclemencon <georgia.clemencon@isen.yncrea.fr>
2024-03-29 10:43:46 +0100

    Afficher des photos

--- app/src/main/java/fr/isen/mouillot/sportscape/DetailPostActivity.kt
+++ app/src/main/java/fr/isen/mouillot/sportscape/DetailPostActivity.kt
@@ -1,6 +1,7 @@
 package fr.isen.mouillot.sportscape
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -28,18 +29,49 @@ import kotlin.coroutines.suspendCoroutine
 class DetailPostActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val postId = intent.getStringExtra("postId")
 
         setContent {
             SportScapeTheme {
-                DetailPostScreen(postId ?: "")
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+//                    DetailPostScreen(postId ?: "")
                 }
+            }
+        }
+
+        suspend fun getPostDetails(postId: String): Post? {
+            // Get an instance of the Firebase Database
+            val database = FirebaseDatabase.getInstance()
+
+            // Create a reference to the location in the database where the post details are stored
+            val postRef = database.getReference("posts/$postId")
+
+            // Use a coroutine to wait for the data to be fetched
+            return withContext(Dispatchers.IO) {
+                val snapshot = suspendCoroutine<DataSnapshot> { continuation ->
+                    // Attach a listener to the reference to retrieve the data
+                    postRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(dataSnapshot: DataSnapshot) {
+                            // Pass the data to the continuation when it's fetched
+                            continuation.resume(dataSnapshot)
+                        }
+
+                        override fun onCancelled(databaseError: DatabaseError) {
+                            // Pass an exception to the continuation if the fetch is cancelled
+                            continuation.resumeWithException(databaseError.toException())
+                        }
+                    })
+                }
+
+                // Convert the snapshot to a Post object and return it
+                snapshot.getValue(Post::class.java)
             }
         }
     }
@@ -50,7 +82,7 @@ fun DetailPostScreen(postId: String) {
     val post = remember { mutableStateOf<Post?>(null) }
 
     LaunchedEffect(postId) {
-        post.value = getPostDetails(postId)
+//        post.value = getPostDetails(postId)
     }
 
     post.value?.let {
@@ -61,34 +93,7 @@ fun DetailPostScreen(postId: String) {
     }
 }
 
-suspend fun getPostDetails(postId: String): Post? {
-    // Get an instance of the Firebase Database
-    val database = FirebaseDatabase.getInstance()
 
-    // Create a reference to the location in the database where the post details are stored
-    val postRef = database.getReference("posts/$postId")
-
-    // Use a coroutine to wait for the data to be fetched
-    return withContext(Dispatchers.IO) {
-        val snapshot = suspendCoroutine<DataSnapshot> { continuation ->
-            // Attach a listener to the reference to retrieve the data
-            postRef.addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    // Pass the data to the continuation when it's fetched
-                    continuation.resume(dataSnapshot)
-                }
-
-                override fun onCancelled(databaseError: DatabaseError) {
-                    // Pass an exception to the continuation if the fetch is cancelled
-                    continuation.resumeWithException(databaseError.toException())
-                }
-            })
-        }
-
-        // Convert the snapshot to a Post object and return it
-        snapshot.getValue(Post::class.java)
-    }
-}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
